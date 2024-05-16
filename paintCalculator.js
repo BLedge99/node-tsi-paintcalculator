241750ed52cb4cc0acf6c051f4906045ce254986
BLedge99 <132830755+BLedge99@users.noreply.github.com>
2024-05-16 18:21:18 +0100

    Update paintCalculator.js
    -Added ability for user to specify obstacles in their walls, for example a door
    -Moved some logic out of userForm to provide more modularity and avoid copy pasting lines for obstacle calculations
    
    Need to add paint can calculations
    GUI would be nice

--- paintCalculator.js
+++ paintCalculator.js
@@ -1,35 +1,42 @@
 const readline = require('node:readline');
-const { PassThrough } = require('node:stream');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
-
 
 const sides = { 'Rectangle': 2, 'Circle': 1, 'Triangle': 3 };
 const shapes = ['Rectangle', 'Triangle', 'Circle'];
-const prices = {'Dulux': 
-                    {"Matt White" : 2.40, "Pure Brilliant White Silk Emulsion" : 2.40, "Matt Standard Emerald Glade" : 2.45, "Matt Easycare Willow Emulsion" : 2.45, "Kiwi Crush Silk": 8.80},
-                'Zinsser':
-                    {"Matt White Anti Mold" : 21, "Perma White Satin Anti Mold" : 16.80, "Black Multi Surface AllCoat" : 28},
-                'Berger':
-                    {"Silk Emulsion Lemon Glow" : 8.40, "Navy Blue Non-Drip":22.19, "Quick Dry Matt White": 12.20, "Gloss Non-Drip Russian Red":22.19, "Satin Magnolla":28}
-                }
-
+const prices = {
+    'Dulux': {
+        "Matt White": 2.40,
+        "Pure Brilliant White Silk Emulsion": 2.40,
+        "Matt Standard Emerald Glade": 2.45,
+        "Matt Easycare Willow Emulsion": 2.45,
+        "Kiwi Crush Silk": 8.80
+    },
+    'Zinsser': {
+        "Matt White Anti Mold": 21,
+        "Perma White Satin Anti Mold": 16.80,
+        "Black Multi Surface AllCoat": 28
+    },
+    'Berger': {
+        "Silk Emulsion Lemon Glow": 8.40,
+        "Navy Blue Non-Drip": 22.19,
+        "Quick Dry Matt White": 12.20,
+        "Gloss Non-Drip Russian Red": 22.19,
+        "Satin Magnolla": 28
+    }
+};
 
 function promptUser(question) {
     return new Promise((resolve) => {
         rl.question(question, (answer) => {
-            resolve(answer);
+        resolve(answer);
         });
     });
 }
 
-
-function calculateArea(wall) {
-    const shape = wall[0];
-    const dimensions = wall[1];
-
+function calculateArea(shape, dimensions) {
     if (shape === 'Rectangle') {
         return dimensions[0] * dimensions[1];
     } 
@@ -41,74 +48,102 @@ function calculateArea(wall) {
     else if (shape === 'Circle') {
         return (dimensions[0] / 2) ** 2 * Math.PI;
     }
-  }
-  
+}
 
 function calculatePrice(area, brand, colour) {
     return area * prices[brand][colour];
-  }
-
-
-function showCatalogue(){
-    console.log(prices)
 }
 
+function showCatalogue() {
+    console.log(prices);
+}
+
+function shapeInstructions(shape,area){
+    switch (shape){
+        case 'Rectangle':
+            console.log(`Please enter the width and height of your ${area}, in meters, one after another: `);
+            break;
+        case 'Triangle':
+            console.log(`Please enter the length of all three sides of your ${area}, in meters, one after another: `);
+            break;
+        case 'Circle':
+            console.log(`Please enter the diameter of your ${area} in meters: `);
+            break;
+    }
+}
+
+async function getShape(area) {
+    let shape = await promptUser(`Please enter shape of ${area} (Rectangle, Triangle, Circle): `);
+    if (!shapes.includes(shape)) {
+        console.log("Please ensure you enter Rectangle, Triangle, or Circle, checking for capitalization.");
+        console.log("Unfortunately, other shapes are not available at this time.");
+        return null;
+    }
+    return shape;
+}
+
+async function getAreas(shape) {
+    let dimensions = [];
+    for (let side = 0; side < sides[shape]; side++) {
+        dimensions.push(parseFloat(await promptUser(`Please enter side ${side + 1}: `)));
+    }
+    return dimensions;
+}
 
 async function userForm() {
     let walls = [];
     const numWalls = parseInt(await promptUser("Please enter the number of walls you would like to paint: "));
     console.log("You will now be asked to enter details about each wall you would like to paint");
-   
+
     //Iterate over each wall to be painted
     for (let i = 0; i < numWalls; i++) {
         console.log(`Please enter details about wall ${i + 1}: `);
         //Get shape of wall
-        let shape = await promptUser("Please enter shape of wall (Rectangle, Triangle, Circle): ");
-        if (!shapes.includes(shape)) {
-            console.log("Please ensure you enter Rectangle, Triangle, or Circle, checking for capitalization.");
-            console.log("Unfortunately, other shapes are not available at this time.");
-            break;
-        }
-      
-        let dimensions = [];
+        let shape = await getShape('wall');
+        if (!shape) continue;
+        //Find wall dimensions
         console.log("Next, you will be asked to enter the dimensions of your wall: ");
-
-        switch (shape){
-            case 'Rectangle':
-                console.log("Please enter the width and height of your wall, in meters, one after another: ");
-                break;
-            case 'Triangle':
-                console.log("Please enter the length of all three sides of your wall, in meters, one after another: ");
-                break;
-            case 'Circle':
-                console.log("Please enter the diameter of your wall in meters: ");
-                break;
+        shapeInstructions(shape,"wall")
+        let dimensions = await getAreas(shape);
+        //Get brand and colour preferences
+        const viewCatalogue = await promptUser("You will now be asked for your paint preferences, if you need to view the paint catalogue please type 1, otherwise press enter: ");
+        if (viewCatalogue === '1') {
+            showCatalogue();
+        }
+        let brand = await promptUser("Please enter brand of paint you would like: ");
+        let colour = await promptUser("Please enter colour of paint you would like: ");
+        //Get any area the user might not want to paint, for example a door
+        let takeAwayShape = 'Circle';
+        let takeAwayDimensions = [0];
+        let takeAwayBool = await promptUser("You will now be asked for the details of any obstructions in your wall, for example a door, if you have any obstructions please type 1, otherwise press enter: ");
+        if (takeAwayBool === '1') {
+            takeAwayShape = await getShape('obstruction');
+        if (!takeAwayShape) continue;
+            shapeInstructions(shape,"obstruction")
+            takeAwayDimensions = await getAreas(takeAwayShape);
         }
 
-        for (let side = 0; side < sides[shape]; side++) {
-            dimensions.push(parseInt(await promptUser(`Please enter side ${side + 1}: `)));
-        }
-
-        const viewCatalogue = await promptUser("You will now be asked for your paint preferences, if you need to view the paint catalogue please type 1, otherwise press enter")
-        if (viewCatalogue === '1'){
-            showCatalogue()
-        }
-        let brand = await promptUser("Please enter brand of paint you would like: ")
-        let colour = await promptUser("Please enter colour of paint you would like: ")
-        walls.push([shape, dimensions,brand,colour]);
+        walls.push([shape, dimensions, brand, colour, takeAwayShape, takeAwayDimensions]);
     }
 
     console.log(walls);
     rl.close();
     return walls;
-  }
-
-
-async function getQuote() {
-    const walls = await userForm();  
-    for (let wall of walls){
-        console.log(`Wall ${wall} will cost £${calculatePrice(calculateArea(wall), wall[2], wall[3])}`)
-    }
 }
 
-getQuote()
+async function getQuote() {
+    const walls = await userForm();
+    for (let wall of walls) {
+      const area = calculateArea(wall[0], wall[1]);
+      const takeAwayArea = calculateArea(wall[4], wall[5]);
+      let cost = calculatePrice(area, wall[2], wall[3]) - calculatePrice(takeAwayArea, wall[2], wall[3]);
+      if  (cost < 0){
+        console.log("Given obstacle is larger than given area to paint, thus cost will be £0")
+        cost = 0
+      }
+      console.log(`Wall ${wall[0]} will cost £${cost.toFixed(2)}`);
+    }
+  }
+
+getQuote();
+
